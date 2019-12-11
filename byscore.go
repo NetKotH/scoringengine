137424b42b7eed0e474958527f59d10d0fe6c7c7
Matt Robinson <brimstone@the.narro.ws>
2019-12-11 17:25:00 -0500

    Missed a file

---
+++ byscore.go
@@ -0,0 +1,7 @@
+package main
+
+type ByScore []TeamScore
+
+func (a ByScore) Len() int           { return len(a) }
+func (a ByScore) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByScore) Less(i, j int) bool { return a[i].Score > a[j].Score }
